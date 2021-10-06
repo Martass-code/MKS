2386e1214fa75e49de03701ef813d25b625d56f1
Martass-code <xelias19@vutbr.cz>
2021-10-06 14:05:12 +0200

    CV2 2.2 externi preruseni

---
+++ workspace_1.7.0/cviceni2/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/Users/marta/Documents/MKS/Include/core_cm0.h \
+ C:/Users/marta/Documents/MKS/Include/core_cmInstr.h \
+ C:/Users/marta/Documents/MKS/Include/core_cmFunc.h \
+ C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+
+C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+C:/Users/marta/Documents/MKS/Include/core_cm0.h:
+
+C:/Users/marta/Documents/MKS/Include/core_cmInstr.h:
+
+C:/Users/marta/Documents/MKS/Include/core_cmFunc.h:
+
+C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
---
+++ workspace_1.7.0/cviceni2/Debug/Src/system_stm32f0xx.d
@@ -0,0 +1,19 @@
+Src/system_stm32f0xx.o: ../Src/system_stm32f0xx.c \
+ C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/Users/marta/Documents/MKS/Include/core_cm0.h \
+ C:/Users/marta/Documents/MKS/Include/core_cmInstr.h \
+ C:/Users/marta/Documents/MKS/Include/core_cmFunc.h \
+ C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+
+C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+C:/Users/marta/Documents/MKS/Include/core_cm0.h:
+
+C:/Users/marta/Documents/MKS/Include/core_cmInstr.h:
+
+C:/Users/marta/Documents/MKS/Include/core_cmFunc.h:
+
+C:/Users/marta/Documents/MKS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
