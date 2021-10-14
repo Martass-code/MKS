c9530c5bb0fd56fcf165c16c6c80181cca59e8bc
Martass-code <xelias19@vutbr.cz>
2021-10-14 08:11:09 +0200

    CV4 2.2 Konfigurace ADC a zobrazeni

---
+++ workspace_1.7.0/cviceni4/Core/Inc/sct.h
@@ -0,0 +1,27 @@
+/*
+ * sct.h
+ *
+ *  Created on: 6. 10. 2021
+ *      Author: Elias
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+#include <stdint.h>
+
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+
+
+void sct_init(void); //povoleni hodin prislusnych portu (RCC_AHBENR_GPIOB), nastaveni pinu jako vystupnich (GPIO_MODER_MODERx_0),
+// zapis nul do posuvneho registru (volání sct_led(0)), aktivování výstupu pomocí /OE (volání sct_noe(0))
+
+void sct_led(uint32_t value); //zapise hodnoty na displej
+
+void sct_value(uint16_t value, uint8_t led); //preklad cislovek na odpovidajici segmenty // led zobrazeni led 0 - 8 LED
+
+#endif /* SCT_H_ */
